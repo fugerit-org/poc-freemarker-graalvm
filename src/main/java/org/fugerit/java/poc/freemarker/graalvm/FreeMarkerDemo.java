b8164ae70426f87d48f1bd40461684882b94a4c7
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2025-05-13 08:35:48 +0200

    Refactor with KO template

--- src/main/java/org/fugerit/java/poc/freemarker/graalvm/FreeMarkerDemo.java
+++ src/main/java/org/fugerit/java/poc/freemarker/graalvm/FreeMarkerDemo.java
@@ -1,5 +1,6 @@
 package org.fugerit.java.poc.freemarker.graalvm;
 
+import freemarker.log.Logger;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
@@ -12,23 +13,35 @@ import java.util.Map;
 
 public class FreeMarkerDemo {
 
+    private final static Logger LOG = Logger.getLogger(FreeMarkerDemo.class.getName());
+
     public static void main(String[] args) {
         FreeMarkerDemo demo = new FreeMarkerDemo();
+        // test a template whose processing will fail
+        demo.runKoTemplate();
+        // test a template whose processing succeeds
         demo.runVersionTemplate();
     }
 
     public void runVersionTemplate() {
+        this.runDefault( "version.ftl" );
+    }
+
+    public void runKoTemplate() {
+        this.runDefault( "ko.ftl" );
+    }
+
+    private void runDefault( String templateName ) {
         try ( StringWriter writer = new StringWriter() ) {
             Map<String, Object> dataModel = new HashMap<>();
-            this.testTemplate( writer, "version.ftl", dataModel );
-            System.out.println( writer );
+            this.testTemplate( writer, templateName, dataModel );
+            LOG.info( "OK, template : %n%s".formatted( writer.toString() ) );
         } catch (Throwable t) {
-            System.err.println( t.getMessage() );
-            t.printStackTrace();
+            LOG.error( "KO, Fatal error : %s".formatted( t ), t );
         }
     }
 
-    public void testTemplate(Writer writer, String templatePath, Map<String, Object> dataModel) throws IOException, TemplateException {
+    private void testTemplate(Writer writer, String templatePath, Map<String, Object> dataModel) throws IOException, TemplateException {
         Configuration cfg = new Configuration( Configuration.VERSION_2_3_34 );
         cfg.setClassForTemplateLoading( FreeMarkerDemo.class, "/templates" );
         Template template = cfg.getTemplate( templatePath );
---
+++ src/main/resources/templates/ko.ftl
@@ -0,0 +1,1 @@
+<ko>${variableNotExists}</ko>
